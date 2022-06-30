11fa74ae16fb9a8a76d685cd089b6b2ee426f0c7
nurkholis <nur.kholis@ist.id>
2022-06-30 13:41:30 +0700

    Initial Commit

---
+++ latihan-swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  latihan-swiftUI
+//
+//  Created by nur kholis on 30/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ latihan-swiftUI/latihan_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  latihan_swiftUIApp.swift
+//  latihan-swiftUI
+//
+//  Created by nur kholis on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct latihan_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
